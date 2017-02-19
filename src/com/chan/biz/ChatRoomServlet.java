f07260cd519f9460b3d6e16c9e8ca2d58d986645
jiacheng <jiacheng.li@shanbay.com>
2017-02-19 14:53:21 +0800

    sync

--- src/com/chan/biz/ChatRoomServlet.java
+++ src/com/chan/biz/ChatRoomServlet.java
@@ -1,15 +1,17 @@
 package com.chan.biz;
 
-import org.jivesoftware.smack.AccountManager;
+
 import org.jivesoftware.smack.ConnectionConfiguration;
 import org.jivesoftware.smack.XMPPConnection;
-import org.jivesoftware.smack.XMPPException;
+import org.jivesoftware.smackx.muc.HostedRoom;
+import org.jivesoftware.smackx.muc.MultiUserChat;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Collection;
 
 /**
  * Created by chan on 17/2/15.
@@ -26,6 +28,13 @@ public class ChatRoomServlet extends HttpServlet {
         try {
             xmppConnection.connect();
             xmppConnection.login("servlet", "1234", "192.168.1.101");
+
+            MultiUserChat.getHostedRooms(xmppConnection, "192.168.1.101");
+//            Collection<HostedRoom> hostrooms;
+//            hostrooms = MultiUserChat.getHostedRooms(xmppConnection, "192.168.1.101");
+//            for (HostedRoom entry : hostrooms) {
+//                System.out.println(entry.getJid() + "->" + entry.getName());
+//            }
         } catch (Exception e) {
             e.printStackTrace();
         }
