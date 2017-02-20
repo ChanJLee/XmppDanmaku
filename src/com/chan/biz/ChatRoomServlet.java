8eae8d752b76f5a61a5a6e419a958c8746fb50c0
jiacheng <jiacheng.li@shanbay.com>
2017-02-20 21:10:08 +0800

    sync

--- src/com/chan/biz/ChatRoomServlet.java
+++ src/com/chan/biz/ChatRoomServlet.java
@@ -1,17 +1,26 @@
 package com.chan.biz;
 
 
+import com.chan.xmpp.XmppHelper;
+import org.jivesoftware.smack.ChatManager;
 import org.jivesoftware.smack.ConnectionConfiguration;
 import org.jivesoftware.smack.XMPPConnection;
+import org.jivesoftware.smack.XMPPException;
+import org.jivesoftware.smackx.Form;
+import org.jivesoftware.smackx.ServiceDiscoveryManager;
 import org.jivesoftware.smackx.muc.HostedRoom;
 import org.jivesoftware.smackx.muc.MultiUserChat;
+import org.jivesoftware.smackx.packet.DiscoverItems;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
 
 /**
  * Created by chan on 17/2/15.
@@ -23,19 +32,49 @@ public class ChatRoomServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        ConnectionConfiguration connectionConfiguration = new ConnectionConfiguration("192.168.1.101", 5222);
-        XMPPConnection xmppConnection = new XMPPConnection(connectionConfiguration);
-        try {
-            xmppConnection.connect();
-            xmppConnection.login("servlet", "1234", "192.168.1.101");
-
-            MultiUserChat.getHostedRooms(xmppConnection, "192.168.1.101");
+//        ConnectionConfiguration connectionConfiguration = new ConnectionConfiguration("192.168.1.101", 5222);
+//        XMPPConnection xmppConnection = new XMPPConnection(connectionConfiguration);
+//        try {
+//            xmppConnection.connect();
+//            xmppConnection.login("servlet", "1234");
+//            MultiUserChat multiUserChat = new MultiUserChat(xmppConnection, "servlet_name___foo@conference.192.168.1" +
+//                    ".101");
+//            multiUserChat.create("servlet_nickname");
+//            multiUserChat.sendConfigurationForm(new Form(Form.TYPE_SUBMIT));
 //            Collection<HostedRoom> hostrooms;
 //            hostrooms = MultiUserChat.getHostedRooms(xmppConnection, "192.168.1.101");
 //            for (HostedRoom entry : hostrooms) {
 //                System.out.println(entry.getJid() + "->" + entry.getName());
 //            }
-        } catch (Exception e) {
+//
+//            ArrayList<DiscoverItems.Item> listDiscoverItems = new ArrayList<DiscoverItems.Item>();
+//            // 获得与XMPPConnection相关的ServiceDiscoveryManager
+//            ServiceDiscoveryManager discoManager = ServiceDiscoveryManager
+//                    .getInstanceFor(xmppConnection);
+//
+//            // 获得指定XMPP实体的项目
+//            // 这个例子获得与在线目录服务相关的项目
+//            DiscoverItems discoItems;
+//            try {
+//                discoItems = discoManager.discoverItems("");
+//                // 获得被查询的XMPP实体的要查看的项目
+//                Iterator it = discoItems.getItems();
+//                // 显示远端XMPP实体的项目
+//                while (it.hasNext()) {
+//                    DiscoverItems.Item item = (DiscoverItems.Item) it.next();
+//                    System.out.println(item.getEntityID());
+//                    System.out.println(item.getName());
+//                    listDiscoverItems.add(item);
+//                }
+//            } catch (XMPPException e) {
+//                e.printStackTrace();
+//            }
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
+        try {
+            XmppHelper.createChatRoom("while_hahaha", "test");
+        } catch (XMPPException e) {
             e.printStackTrace();
         }
     }
--- src/com/chan/xmpp/XmppHelper.java
+++ src/com/chan/xmpp/XmppHelper.java
@@ -1,10 +1,65 @@
 package com.chan.xmpp;
 
 
+import org.apache.commons.lang.StringUtils;
+import org.jivesoftware.smack.ConnectionConfiguration;
+import org.jivesoftware.smack.XMPPConnection;
+import org.jivesoftware.smack.XMPPException;
+import org.jivesoftware.smackx.Form;
+import org.jivesoftware.smackx.ServiceDiscoveryManager;
+import org.jivesoftware.smackx.muc.MultiUserChat;
+import org.jivesoftware.smackx.packet.DiscoverItems;
+
+import java.util.Iterator;
+
 /**
  * Created by chan on 17/2/16.
  */
 public class XmppHelper {
-    private static final int XMPP_PORT = 5222;
-    private static final String XMPP_DOMAIN = "localhost";
+    public static final int XMPP_PORT = 5222;
+    public static final String XMPP_DOMAIN = "192.168.1.101";
+    public static final String XMPP_CHAT_ROOM_ID = "conference.192.168.1.101";
+
+    private static volatile XMPPConnection sXmppConnection;
+
+    public static XMPPConnection getXmppConnection() throws XMPPException {
+        if (sXmppConnection == null) {
+            synchronized (XmppHelper.class) {
+                if (sXmppConnection == null) {
+                    ConnectionConfiguration connectionConfiguration = new ConnectionConfiguration(XMPP_DOMAIN, XMPP_PORT);
+                    sXmppConnection = new XMPPConnection(connectionConfiguration);
+                }
+            }
+        }
+
+        if (!sXmppConnection.isConnected()) {
+            sXmppConnection.connect();
+            sXmppConnection.login("admin", "19940525");
+        }
+
+        return sXmppConnection;
+    }
+
+    public static String queryChatRoom(String name) throws XMPPException {
+        XMPPConnection connection = getXmppConnection();
+        ServiceDiscoveryManager discoManager = ServiceDiscoveryManager.getInstanceFor(connection);
+        DiscoverItems discoItems = discoManager.discoverItems(XMPP_CHAT_ROOM_ID);
+        Iterator<DiscoverItems.Item> it = discoItems.getItems();
+        while (it.hasNext()) {
+            DiscoverItems.Item item = it.next();
+            if (StringUtils.equals(item.getName(), name)) {
+                return item.getEntityID();
+            }
+        }
+
+        return null;
+    }
+
+    public static void createChatRoom(String name, String title) throws XMPPException {
+        XMPPConnection connection = getXmppConnection();
+        System.out.print(String.format("%s@%s", name, XMPP_CHAT_ROOM_ID));
+        MultiUserChat multiUserChat = new MultiUserChat(connection, String.format("%s@%s", name, XMPP_CHAT_ROOM_ID));
+        multiUserChat.create(title);
+        multiUserChat.sendConfigurationForm(new Form(Form.TYPE_SUBMIT));
+    }
 }
